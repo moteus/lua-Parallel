9bb3eacfbd361aeb8a71e68d1e3d5c37bfabc254
Alexey Melnichuk <mimir@newmail.ru>
2014-02-05 11:47:17 +0400

    Update example.

--- examples/hash.lua
+++ examples/hash.lua
@@ -5,6 +5,23 @@ local MASK    = [[*.*]]
 local THREADS = 4
 local RECURSE = true
 
+local HASH_FILE = string.dump(function()
+  require "digest"
+  local CHUNK_SIZE = 4096
+  TASK(function(fname)
+    local f, err = io.open(fname,"rb")
+    if not f then return fname, nil, err end
+    local d = md5.new()
+    while true do
+      local c = f:read(CHUNK_SIZE)
+      if c==nil then break end
+      d:update(c)
+    end
+    f:close()
+    return fname, d:digest()
+  end)
+end)
+
 local function WRITE_HASH(fname, hash, err)
   if hash then
     io.stdout:write( string.lower(hash), ' *', fname, '\n' )
@@ -40,22 +57,5 @@ FILES = function(P)
 end
 
 end
-
-local HASH_FILE = string.dump(function()
-  require "digest"
-  local CHUNK_SIZE = 4096
-  TASK(function(fname)
-    local f, err = io.open(fname,"rb")
-    if not f then return nil, err end
-    local d = md5.new()
-    while true do
-      local c = f:read(CHUNK_SIZE)
-      if c==nil then break end
-      d:update(c)
-    end
-    f:close()
-    return fname, d:digest()
-  end)
-end)
 
 Parallel.ForEach(FILES(MASK), HASH_FILE, WRITE_HASH, THREADS)
